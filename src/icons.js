7419dfbe3e18da79a5eaa726deda80a8dd20dc17
huluvu424242 <huluvu424242@gmail.com>
2026-04-19 14:18:43 +0200

    eye button migriert

--- src/icons.js
+++ src/icons.js
@@ -122,6 +122,19 @@ export const ICONS = Object.freeze({
     </svg>
   `,
     /*
+     * Icon: eye
+     * Copyright: Huluvu424242
+     * Lizenz: MIT
+     * Quelle: selbst erstellt
+     */
+    eye: `
+    <svg class="icon" viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M2 12s3.8-6 10-6 10 6 10 6-3.8 6-10 6-10-6-10-6z" />
+        <circle cx="12" cy="12" r="2.8" />
+        <path class="eye-slash" d="M4 4l16 16" />
+    </svg>
+  `,
+    /*
      * Icon: speaking_head
      * Copyright: Twitter
      * Lizenz: MIT
