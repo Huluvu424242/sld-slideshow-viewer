924d528965f853b4d07afed583dee0d436b36d09
huluvu424242 <huluvu424242@gmail.com>
2026-04-23 19:10:28 +0200

    class icon gesetzt

--- src/icons.js
+++ src/icons.js
@@ -499,6 +499,7 @@ export const ICONS = Object.freeze({
     error_info: `
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      class="icon"
       viewBox="0 0 24 24"
       aria-hidden="true"
     >
